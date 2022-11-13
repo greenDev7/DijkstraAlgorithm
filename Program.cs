e045710684e4729103c5067ed92c0fa7bf0df8b2
Зелёный Андрей Сергеевич <andrey.zelenyy@south.rt.ru>
2022-11-13 21:56:24 +0300

    Рефакторинг метода Main() для работы с препятствиями. Добавлен метод для запись кратчайшего пути в файл txt - WriteShortestPathToFile()

--- Program.cs
+++ Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DijkstraAlgorithm
 {
@@ -10,19 +9,33 @@ namespace DijkstraAlgorithm
     {
         static void Main(string[] args)
         {
-            Graph graph = new Graph(1, 1, 2, 3, Surface.Plane);
+            string directoryPath = Environment.CurrentDirectory;
+            int[,] obstacleMatrix = Obstacle.CreateObstacleMatrixFromCSVFile(directoryPath + "Maze1.csv");
+            Obstacle.WriteObstacleToFile(obstacleMatrix, directoryPath + "Maze1Obstacle.txt");
 
-            graph.Vertices[0, 1].IsObstacle = true;
-            graph.Vertices[1, 2].IsObstacle = true;
+            //Graph graph = new Graph(1, 1, 2, 3, Surface.Plane);
+            Graph graph = new Graph(obstacleMatrix);
+
 
             double shortestPathLength;
 
-            Vertex start = graph.Vertices[0, 0];
-            Vertex goal = graph.Vertices[0, 2];
+            Point2D startPoint = new Point2D(4, 46);
+            Point2D goalPoint = new Point2D(95, 4);
 
-            List<Point2D> path = graph.FindShortestPathAndLength(start, goal, out shortestPathLength);            
+            List<Point2D> path = graph.FindShortestPathAndLength(startPoint, goalPoint, out shortestPathLength);
+
+            WriteShortestPathToFile(path, directoryPath + "Maze1ShortestPath.txt");
 
             Console.ReadLine();
+        }
+
+        private static void WriteShortestPathToFile(List<Point2D> shortestPath, string fileName)
+        {
+            List<string> lines = new List<string>();
+            foreach (Point2D point in shortestPath)
+                lines.Add(string.Format("{0};{1};1", point.i, point.j));
+
+            File.WriteAllLines(fileName, lines);
         }
     }
 }
