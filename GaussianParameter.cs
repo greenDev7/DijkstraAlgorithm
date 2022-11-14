a14fa319c8b4015932140a3186f8a9a2a8f92e7e
Зелёный Андрей Сергеевич <andrey.zelenyy@south.rt.ru>
2022-11-15 02:01:17 +0300

    Добавлен вспомогательный класс для хранения параметров Гауссиана

---
+++ GaussianParameter.cs
@@ -0,0 +1,38 @@
+﻿namespace DijkstraAlgorithm
+{
+    /// <summary>
+    /// Вспомогательный класс для хранения параметров для вычисления двумерного Гауссиана
+    /// </summary>
+    public class GaussianParameter
+    {
+        /// <summary>
+        /// Высота колокола
+        /// </summary>
+        public double A { get; }
+        /// <summary>
+        /// Размах колокола по оси Ox
+        /// </summary>
+        public double sigmaX { get; }
+        /// <summary>
+        /// Размах колокола по оси Oy
+        /// </summary>
+        public double sigmaY{ get; }
+        /// <summary>
+        /// Сдвиг пика по оси Ox
+        /// </summary>
+        public double x0 { get; }
+        /// <summary>
+        /// Сдвиг пика по оси Oy
+        /// </summary>
+        public double y0 { get; }
+
+        public GaussianParameter(double A, double sigmaX, double sigmaY, double x0, double y0)
+        {
+            this.A = A;
+            this.sigmaX = sigmaX;
+            this.sigmaY = sigmaY;
+            this.x0 = x0;
+            this.y0 = y0;
+        }
+    }
+}
