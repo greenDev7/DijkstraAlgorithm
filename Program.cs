5d20415a25466c262b197777a2f60afa62fc1733
Зелёный Андрей Сергеевич <andrey.zelenyy@south.rt.ru>
2022-11-15 02:01:59 +0300

    Рефакторинг метода Main() для поиска кратчайшего пути на поверхности

--- Program.cs
+++ Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 
 namespace DijkstraAlgorithm
 {
@@ -9,22 +8,37 @@ namespace DijkstraAlgorithm
     {
         static void Main(string[] args)
         {
-            string directoryPath = Environment.CurrentDirectory;
-            int[,] obstacleMatrix = Obstacle.CreateObstacleMatrixFromCSVFile(directoryPath + "Maze1.csv");
-            Obstacle.WriteObstacleToFile(obstacleMatrix, directoryPath + "Maze1Obstacle.txt");
+            // Создаем несколько экземпляров параметров для Гауссиана для имитации гор (холмов) и одного оврага
+            GaussianParameter gaussianParameter1 = new GaussianParameter(1.5, 0.5, 0.5, 2.0, 4.0);
+            GaussianParameter gaussianParameter2 = new GaussianParameter(1.0, 0.5, 0.5, 7.5, 1.0);
+            GaussianParameter gaussianParameter3 = new GaussianParameter(-0.5, 0.2, 1.0, 5.0, 0.5);
+            GaussianParameter gaussianParameter4 = new GaussianParameter(1.0, 0.5, 0.8, 3.5, 2.2);
 
-            //Graph graph = new Graph(1, 1, 2, 3, Surface.Plane);
-            Graph graph = new Graph(obstacleMatrix);
+            // Инициализируем граф
+            Graph graph = new Graph(0.1, 0.1, 101, 51, 20.0, gaussianParameter1, gaussianParameter2, gaussianParameter3, gaussianParameter4);
 
+            // Создаем искуственные сооружения на карте
+            graph.CreateBuilding(new Point2D(57, 20), 2, 20, 0.3);
+            graph.CreateBuilding(new Point2D(64, 16), 3, 5, 0.3);
+            graph.CreateBuilding(new Point2D(18, 4), 2, 5, 0.3);
+            graph.CreateBuilding(new Point2D(10, 14), 4, 2, 0.4);
+            graph.CreateBuilding(new Point2D(64, 29), 5, 2, 0.4);
+            graph.CreateBuilding(new Point2D(14, 24), 5, 2, 0.3);
 
-            double shortestPathLength;
+            // Записываем получившуюся поверхность в файл
+            string docPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            graph.WriteSurfaceToFile(Path.Combine(docPath, "surface.txt"));
 
-            Point2D startPoint = new Point2D(4, 46);
-            Point2D goalPoint = new Point2D(95, 4);
+            // Вычисляем кратчайший путь
+            double shortestPathLength = 0.0;
 
-            List<Point2D> path = graph.FindShortestPathAndLength(startPoint, goalPoint, out shortestPathLength);
+            Point2D startPoint = new Point2D(92, 7);
+            Point2D goalPoint = new Point2D(14, 21);
 
-            WriteShortestPathToFile(path, directoryPath + "Maze1ShortestPath.txt");
+            List<Point2D> shortestPath = graph.FindShortestPathAndLength(startPoint, goalPoint, out shortestPathLength);
+
+            // Записываем найденный путь в файл
+            WriteShortestPathToFile(shortestPath, graph, Path.Combine(docPath, "shortestPath.txt"));
 
             Console.ReadLine();
         }
@@ -34,6 +48,15 @@ namespace DijkstraAlgorithm
             List<string> lines = new List<string>();
             foreach (Point2D point in shortestPath)
                 lines.Add(string.Format("{0};{1};1", point.i, point.j));
+
+            File.WriteAllLines(fileName, lines);
+        }
+
+        private static void WriteShortestPathToFile(List<Point2D> shortestPath, Graph graph, string fileName)
+        {
+            List<string> lines = new List<string>();
+            foreach (Point2D point in shortestPath)
+                lines.Add(string.Format("{0};{1};{2}", point.i * 0.1, point.j * 0.1, graph.Vertices[point.i, point.j].Height));
 
             File.WriteAllLines(fileName, lines);
         }
