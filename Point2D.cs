81b5091329f9d5f9e369c4e0b53b9e69a107d12e
Зелёный Андрей Сергеевич <andrey.zelenyy@south.rt.ru>
2022-11-15 21:19:56 +0300

    Убрал за ненадобностью set-аксессоры из свойств класса Point2D

--- Point2D.cs
+++ Point2D.cs
@@ -2,8 +2,8 @@
 {
     public class Point2D
     {
-        public int i { get; set; }
-        public int j { get; set; }
+        public int i { get; }
+        public int j { get; }
 
         public Point2D(int i, int j)
         {
