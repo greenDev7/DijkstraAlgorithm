adcd7c52a36fd077fbe570640dc26f644798dee3
Зелёный Андрей Сергеевич <andrey.zelenyy@south.rt.ru>
2022-11-16 18:44:34 +0300

    Добавил отдельную ветку для инициализации графа с помощью матрицы-препятствий из csv-файла

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace DijkstraAlgorithm
 {
@@ -8,37 +9,23 @@ namespace DijkstraAlgorithm
     {
         static void Main(string[] args)
         {
-            // Создаем несколько экземпляров параметров для Гауссиана для имитации гор (холмов) и одного оврага
-            GaussianParameter gaussianParameter1 = new GaussianParameter(1.5, 0.5, 0.5, 2.0, 4.0);
-            GaussianParameter gaussianParameter2 = new GaussianParameter(1.0, 0.5, 0.5, 7.5, 1.0);
-            GaussianParameter gaussianParameter3 = new GaussianParameter(-0.5, 0.2, 1.0, 5.0, 0.5);
-            GaussianParameter gaussianParameter4 = new GaussianParameter(1.0, 0.5, 0.8, 3.5, 2.2);
-
-            // Инициализируем граф
-            Graph graph = new Graph(0.1, 0.1, 101, 51, 20.0, gaussianParameter1, gaussianParameter2, gaussianParameter3, gaussianParameter4);
-
-            // Создаем искуственные сооружения на карте
-            graph.CreateBuilding(new Point2D(57, 20), 2, 20, 0.3);
-            graph.CreateBuilding(new Point2D(64, 16), 3, 5, 0.3);
-            graph.CreateBuilding(new Point2D(18, 4), 2, 5, 0.3);
-            graph.CreateBuilding(new Point2D(10, 14), 4, 2, 0.4);
-            graph.CreateBuilding(new Point2D(64, 29), 5, 2, 0.4);
-            graph.CreateBuilding(new Point2D(14, 24), 5, 2, 0.3);
-
-            // Записываем получившуюся поверхность в файл
+            // Создаем матрицу-препятствий из csv-файла
             string docPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-            graph.WriteSurfaceToFile(Path.Combine(docPath, "surface.txt"));
+            int[,] obstacleMatrix = Obstacle.CreateObstacleMatrixFromCSVFile(Path.Combine(docPath, "MazeMini.csv"));
+
+            // Инициализируем граф с помощью этой матрицы
+            Graph graph = new Graph(obstacleMatrix);
 
             // Вычисляем кратчайший путь
             double shortestPathLength = 0.0;
 
-            Point2D startPoint = new Point2D(92, 7);
-            Point2D goalPoint = new Point2D(14, 21);
+            Point2D startPoint = new Point2D(2, 12);
+            Point2D goalPoint = new Point2D(6, 7);
 
             List<Point2D> shortestPath = graph.FindShortestPathAndLength(startPoint, goalPoint, out shortestPathLength);
 
             // Записываем найденный путь в файл
-            WriteShortestPathToFile(shortestPath, graph, Path.Combine(docPath, "shortestPath.txt"));
+            WriteShortestPathToFile(shortestPath, Path.Combine(docPath, "shortestPath.txt"));
 
             Console.ReadLine();
         }
