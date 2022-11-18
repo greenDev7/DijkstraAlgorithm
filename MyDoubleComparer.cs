5b346e9970751b5a854579d55c4f1804553a2e0f
Зелёный Андрей Сергеевич <andrey.zelenyy@south.rt.ru>
2022-11-19 02:11:11 +0300

    Добавлена библиотека ConcurrentPriorityQueue для использования очереди с приоритетом. Добавлен класс MyDoubleComparer реализующий IComparer<double> для сравнения двух вещественных чисел

---
+++ MyDoubleComparer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace DijkstraAlgorithm
+{
+    /// <summary>
+    /// Реализует интерфейс сравнения двух вещественных чисел
+    /// </summary>
+    class MyDoubleComparer : IComparer<double>
+    {
+        public int Compare(double x, double y)
+        {
+            if (x < y)
+                return 1;
+            if (x > y)
+                return -1;
+            else
+                return 0;
+        }
+    }
+}
