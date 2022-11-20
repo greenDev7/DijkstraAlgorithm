c3f2d45a1de7c1e9e025f87bf4b57809cd26944f
Зелёный Андрей Сергеевич <andrey.zelenyy@south.rt.ru>
2022-11-20 22:29:32 +0300

    Изменен Main()

--- Program.cs
+++ Program.cs
@@ -9,25 +9,29 @@ namespace DijkstraAlgorithm
     {
         static void Main(string[] args)
         {
-            GaussianParameter gaussianParameter1 = new GaussianParameter(1.5, 0.5, 0.5, 5.0, 2.5);
-
-            // Инициализируем граф
-            Graph graph = new Graph(0.1, 0.1, 101, 51, 0.1, gaussianParameter1);
-
-            // Записываем получившуюся поверхность в файл
+            // Создаем матрицу-препятствий из csv-файла
             string docPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-            graph.WriteSurfaceToFile(Path.Combine(docPath, "surface.txt"));
+            int[,] obstacleMatrix = Obstacle.CreateObstacleMatrixFromCSVFile(Path.Combine(docPath, "obstacle1.csv"));
+
+            // Инициализируем граф с помощью этой матрицы
+            Graph graph = new Graph(obstacleMatrix);
 
             // Вычисляем кратчайший путь
             double shortestPathLength = 0.0;
 
-            Point2D startPoint = new Point2D(85, 25);
-            Point2D goalPoint = new Point2D(15, 25);
+            Point2D startPoint = new Point2D(3, 4);
+            Point2D goalPoint = new Point2D(12, 4);
 
             List<Point2D> shortestPath = graph.FindShortestPathAndLength(startPoint, goalPoint, out shortestPathLength);
 
             // Записываем найденный путь в файл
-            WriteShortestPathToFile(shortestPath, graph, Path.Combine(docPath, "shortestPath_max_slope_0_1_degrees.txt"));
+            //WriteShortestPathToFile(shortestPath, Path.Combine(docPath, "shortestPath.txt"));
+
+            Console.WriteLine("Coordinates of shortest path: ");
+            foreach (Point2D p in shortestPath)
+                Console.WriteLine(string.Format("({0}, {1})", p.i, p.j));
+
+            Console.WriteLine(string.Format("Length of shortest path: {0}", shortestPathLength));
 
             Console.ReadLine();
         }
